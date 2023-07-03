6f278e08548f5e0defe1c879fc68af2ba74c692a
MikeJMS8910 <61876275+MikeJMS8910@users.noreply.github.com>
2023-07-03 14:24:42 -0400

    Add files via upload

--- content.js
+++ content.js
@@ -1,4 +1,5 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    console.log("message received")
     if (request.action === "startAutofill") {
         const html = document.documentElement.outerHTML
         console.log(html)
--- popup.js
+++ popup.js
@@ -3,6 +3,20 @@ document.addEventListener("DOMContentLoaded", function() {
     autofillBtn.addEventListener("click", function() {
         chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
             chrome.tabs.sendMessage(tabs[0].id, { action: "startAutofill" })
+            autofillBtn.remove()
+            document.querySelector("h2").style.display = "block"
+            let i = 0
+            setInterval((e) => {
+                i += 1
+                if(i == 4) {
+                    i = 0
+                }
+
+                document.querySelector("h2").textContent = "Autofilling"
+                for(let x = 0; x < i; x++) {
+                    document.querySelector("h2").textContent += "."
+                }
+            }, 300)
         })
     })
 })  
