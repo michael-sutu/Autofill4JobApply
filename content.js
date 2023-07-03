ab38557493f4123e4a263703ce93e4cff4b3558a
MikeJMS8910 <61876275+MikeJMS8910@users.noreply.github.com>
2023-07-03 15:05:08 -0400

    Add files via upload

--- content.js
+++ content.js
@@ -1,7 +1,42 @@
+function fill(element) {
+    let elementType = element.nodeName.toLowerCase()
+    let labels = []
+    element.labels.forEach((label) => {
+        labels.push(label.textContent)
+    })
+
+    let elementData = {
+        "type": elementType,
+        "labels": labels
+    }
+
+    if(elementType == "input") {
+        elementData.placeholder = element.placeholder
+        elementData.inputType = element.type
+        elementData.value = element.value
+    } else if(elementType == "select") {
+        let options = []
+        element.options.forEach((option) => {
+            options.push(option.textContent)
+        })
+        elementData.options = options
+    } else if(elementType == "textarea") {
+        elementData.placeholder = element.placeholder
+        elementData.value = element.value
+    }
+
+    console.log(elementData)
+}
+
+// Idea: For values that do not have a question directly linked, start from the element and then move up in the DOM looking for elements with text-content.
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    console.log("message received")
     if (request.action === "startAutofill") {
-        const html = document.documentElement.outerHTML
-        console.log(html)
+        let searchFor = ["input", "textarea", "select"]
+        for(let i = 0; i < searchFor.length; i++) {
+            document.querySelectorAll(searchFor[i]).forEach((element) => {
+                fill(element)
+            })
+        }
     }
 })
