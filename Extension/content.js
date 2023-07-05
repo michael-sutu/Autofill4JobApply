0523cc6bfaac82ee8beccbcf34a58ba8dfaedacb
MikeJMS8910 <61876275+MikeJMS8910@users.noreply.github.com>
2023-07-05 11:05:47 -0400

    Add files via upload

--- Extension/content.js
+++ Extension/content.js
@@ -39,56 +39,64 @@ function shrink(info) {
 }
 
 function fill(element, info) {
-    let elementType = element.nodeName.toLowerCase()
-    let labels = []
-    element.labels.forEach((label) => {
-        labels.push(label.textContent)
-    })
+    try {
+        let elementType = element.nodeName.toLowerCase()
+        let labelList = []
+        if(element.labels) {
+            element.labels.forEach((label) => {
+                labelList.push(label.textContent)
+            })
+        }
+    
+        let elementData = {
+            "type": elementType,
+            "labels": labelList,
+            "nearby": nearby(element)
+        }
 
-    let elementData = {
-        "type": elementType,
-        "labels": labels,
-        "nearby": nearby(element)
-    }
+        if(elementType == "input") {
+            elementData.placeholder = element.placeholder
+            elementData.inputType = element.type
+            elementData.value = element.value
+        } else if(elementType == "select") {
+            let options = []
+            if(element.options) {
+                for(let o = 0; o < element.options.length; o++) {
+                    options.push(element.options[o].textContent)
+                }
+            }
+            elementData.options = options
+        } else if(elementType == "textarea") {
+            elementData.placeholder = element.placeholder
+            elementData.value = element.value
+        }
 
-    if(elementType == "input") {
-        elementData.placeholder = element.placeholder
-        elementData.inputType = element.type
-        elementData.value = element.value
-    } else if(elementType == "select") {
-        let options = []
-        element.options.forEach((option) => {
-            options.push(option.textContent)
-        })
-        elementData.options = options
-    } else if(elementType == "textarea") {
-        elementData.placeholder = element.placeholder
-        elementData.value = element.value
-    }
-
-    for(let i = 0; i < info.length; i++) {
-        for(let x = 0; x < info[i].queries.length; x++) {
-            if(compress(elementData.labels[0]) == info[i].queries[x]) {
-                element.value = info[i].value
-                return 1
-            } else {
-                if(compress(elementData.nearby[0]) == info[i].queries[x]) {
+        for(let i = 0; i < info.length; i++) {
+            for(let x = 0; x < info[i].queries.length; x++) {
+                if(compress(elementData.labels[0]) == info[i].queries[x]) {
                     element.value = info[i].value
                     return 1
                 } else {
-                    if(elementData.placeholder != undefined) {
-                        if(compress(elementData.placeholder) == info[i].queries[x]) {
-                            element.value = info[i].value
-                            return 1
+                    if(compress(elementData.nearby[0]) == info[i].queries[x]) {
+                        element.value = info[i].value
+                        return 1
+                    } else {
+                        if(elementData.placeholder != undefined) {
+                            if(compress(elementData.placeholder) == info[i].queries[x]) {
+                                element.value = info[i].value
+                                return 1
+                            }
                         }
                     }
                 }
             }
         }
-    }
     
-    fetch(`http://localhost:3000/api/unknown?data=${JSON.stringify(elementData)}`)
-    return 0
+        //fetch(`http://localhost:3000/api/unknown?data=${JSON.stringify(elementData)}`)
+        return 0
+    } catch(error) {
+        console.log(error)
+    }
 }
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
@@ -101,12 +109,134 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
                         let searchFor = ["input", "textarea", "select"]
                         let total = 0
                         let filled = 0
+
+                        if(document.getElementById("input-7")) {
+                            document.getElementById("input-7").click()
+                        }
+
                         for(let i = 0; i < searchFor.length; i++) {
                             document.querySelectorAll(searchFor[i]).forEach((element) => {
                                 filled += fill(element, shrink(data.Info))
                                 total += 1
                             })
                         }
+
+                        let state = ""
+                        let start = ""
+                        let end = ""
+                        let degree = ""
+                        let gender = ""
+                        let HorL = ""
+                        let ethnicity = ""
+                        let veteran = ""
+                        for(let i = 0; i < data.Info.length; i++) {
+                            if(data.Info[i].Question == "State") {
+                                state = data.Info[i].Values[0]
+                            } else if(data.Info[i].Question == "Start Work Month") {
+                                start = data.Info[i].Values[0]
+                            } else if(data.Info[i].Question == "End Work Month") {
+                                end = data.Info[i].Values[0]
+                            } else if(data.Info[i].Question == "Degree") {
+                                degree = data.Info[i].Values[0]
+                            } else if(data.Info[i].Question == "Gender") {
+                                gender = data.Info[i].Values[0]
+                            } else if(data.Info[i].Question == "Hispanic or Latino") {
+                                HorL = data.Info[i].Values[0]
+                            } else if(data.Info[i].Question == "Ethnicity") {
+                                ethnicity = data.Info[i].Values[0]
+                            } else if(data.Info[i].Question == "Veteran") {
+                                veteran = data.Info[i].Values[0]
+                            }
+                        }
+
+                        document.querySelectorAll("div").forEach((e) => {
+                            if(compress(e.textContent) == compress(state)) {
+                                e.click()
+                                filled += 1
+                            }
+                        })
+
+                        let r = 0
+                        if(document.querySelector(".css-hdh0bo")) {
+                            document.querySelectorAll(".css-hdh0bo").forEach((e) => {
+                                e.click()
+                                document.querySelectorAll("label").forEach((e) => {
+                                    if(r == 0) {
+                                        if(compress(e.textContent) == compress(start)) {
+                                            e.click()
+                                            filled += 1
+                                        }
+                                    } else if(r == 1) {
+                                        if(compress(e.textContent) == compress(end)) {
+                                            e.click()
+                                            filled += 1
+                                        }
+                                    }
+                                })
+                                r += 1
+                            })
+                        }
+                        
+                        if(document.getElementById("input-37")) {
+                            document.getElementById("input-37").click()
+                            document.querySelectorAll("div").forEach((e) => {
+                                if(compress(e.textContent) == compress(degree)) {
+                                    e.click()
+                                    filled += 1
+                                }
+                            })
+                        }
+
+                        if(document.getElementById("input-117")) {
+                            document.getElementById("input-117").click()
+                            document.querySelectorAll("div").forEach((e) => {
+                                if(compress(e.textContent) == "yes") {
+                                    e.click()
+                                    filled += 1
+                                }
+                            })
+                        }
+
+                        if(document.getElementById("input-122")) {
+                            document.getElementById("input-122").click()
+                            document.querySelectorAll("div").forEach((e) => {
+                                if(compress(e.textContent) == compress(gender)) {
+                                    e.click()
+                                    filled += 1
+                                }
+                            })
+                        }
+
+                        if(document.getElementById("input-123")) {
+                            document.getElementById("input-123").click()
+                            document.querySelectorAll("div").forEach((e) => {
+                                if(compress(e.textContent) == compress(HorL)) {
+                                    e.click()
+                                    filled += 1
+                                }
+                            })
+                        }
+
+                        if(document.getElementById("input-124")) {
+                            document.getElementById("input-124").click()
+                            document.querySelectorAll("div").forEach((e) => {
+                                if(compress(e.textContent) == compress(ethnicity)) {
+                                    e.click()
+                                    filled += 1
+                                }
+                            })
+                        }
+
+                        if(document.getElementById("input-125")) {
+                            document.getElementById("input-125").click()
+                            document.querySelectorAll("div").forEach((e) => {
+                                if(compress(e.textContent) == compress(veteran)) {
+                                    e.click()
+                                    filled += 1
+                                }
+                            })
+                        }
+
                         chrome.runtime.sendMessage({
                             msg: "stopAutofill", 
                             data: {
